9981ebe3554f6ca54afae0677a663d45f77e0f21
rueandit <rueandit@hotmail.com>
2018-03-07 19:29:47 +0800

    updated sql scripts

--- _install/pallet_plus_cafe.sql
+++ _install/pallet_plus_cafe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 26, 2018 at 02:29 PM
+-- Generation Time: Mar 07, 2018 at 12:27 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -34,6 +34,27 @@ CREATE TABLE `customer_table` (
   `description` varchar(1024) NOT NULL,
   `archived` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer_table`
+--
+
+INSERT INTO `customer_table` (`id`, `name`, `description`, `archived`) VALUES
+(1, 'alpha', 'alpha', 1),
+(2, 'bravo', 'bravo', 1),
+(3, 'charlie', 'charlie', 1),
+(4, 'delta', 'delta', 1),
+(5, 'echo', 'echo', 1),
+(6, 'foxtrot', 'foxtrot', 0),
+(7, 'golf', 'golf', 0),
+(8, 'hotel', 'hotel', 0),
+(9, 'india', 'india', 0),
+(10, 'juliet', 'juliet', 0),
+(11, 'kilo', 'kilo', 0),
+(12, 'lima', 'lima', 0),
+(13, 'mike', 'mike', 0),
+(14, 'november', 'november', 0),
+(15, 'test', 'test', 0);
 
 -- --------------------------------------------------------
 
@@ -66,6 +87,13 @@ CREATE TABLE `images` (
   `imageFile` longblob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `name`, `description`, `imageFile`) VALUES
+(1, 'image1', 'Test Image', 0x746573742c6a7067);
+
 -- --------------------------------------------------------
 
 --
@@ -95,6 +123,27 @@ CREATE TABLE `ingredient` (
   `imageId` int(11) NOT NULL,
   `archived` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ingredient`
+--
+
+INSERT INTO `ingredient` (`id`, `name`, `description`, `unit`, `amount`, `imageId`, `archived`) VALUES
+(1, 'Spama', 'Slices of Spams', 'slices', '1000', 1, 1),
+(2, 'Egg', 'Piece of Egg', 'piece', '100', 1, 1),
+(3, 'Rice', 'Cup of rice', 'cup', '100', 1, 0),
+(4, 'Cucumber', 'Cucumber slices', 'piece', '100', 1, 0),
+(5, 'Tomato', 'Slices of Tomato', 'slice', '100', 1, 0),
+(6, 'Tapa', 'Grams of Tapa', 'gram', '1000', 1, 0),
+(7, 'Tocino', 'Grams of Tocino', 'gram', '100', 1, 0),
+(8, 'Hptdog', 'Pieces of Jumbo Hotdogs', 'piece', '100', 1, 0),
+(9, 'Bacon', 'Pieces of BaconBSpam', 'piece', '100', 1, 0),
+(11, 'Buns', 'Pieces of Buns', 'piece', '100', 1, 0),
+(12, 'Mayonaise', 'Tablespoon of Mayonaise', 'tbsp', '100', 1, 0),
+(13, 'Lettuce', 'Lettuce leaves', 'piece', '100', 1, 0),
+(14, 'Burger Patty', 'Pieces of burger patty', 'piece', '100', 1, 0),
+(15, 'Cheese', 'Slice of cheese', 'slice', '100', 1, 0),
+(16, 'test', 'test', 'test', '1', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -142,6 +191,7 @@ CREATE TABLE `menu` (
 --
 
 INSERT INTO `menu` (`id`, `menuName`, `menuDescription`, `menuStatus`, `price`, `rating`, `archived`, `categoryId`, `subCategory`, `allergenId`, `imageFile`) VALUES
+(1, 'Baconsilog', 'Everybody loves bacon, even Filipinos. This meal is more or less a fusion of the American and Filipino cuisine.', 'Available', '95', 'Normal', 0, 1, '', 1, ''),
 (2, 'Tapsilog', 'Tapsilog', 'Not Available', '95', 'Best Seller', 0, 5, 'Regular', 3, '3'),
 (3, 'Hotsilog', 'Hotsilog', 'Available', '95', 'Normal', 1, 1, '', 1, ''),
 (4, 'Tocilog', 'Tocilog', 'Available', '95', 'Normal', 1, 1, '', 1, ''),
@@ -176,15 +226,16 @@ CREATE TABLE `menu_allergen` (
 --
 
 INSERT INTO `menu_allergen` (`id`, `code`, `description`, `archived`) VALUES
-(1, 'milk', 'Milk', 0),
+(1, 'milky', 'Milky', 0),
 (2, 'peanuts', 'Peanuts', 0),
 (3, 'eggs', 'Eggs', 0),
 (4, 'fish', 'Fish', 0),
 (5, 'soy', 'Soy', 0),
-(6, 'wheat', 'Wheat', 0),
+(6, 'wheat', 'Wheat', 1),
 (7, 'treenuts', 'Tree Nuts', 0),
 (8, 'others', 'Others', 0),
-(9, 'none', 'None', 0);
+(9, 'none', 'None', 0),
+(10, 'test', 'test', 0);
 
 -- --------------------------------------------------------
 
@@ -222,7 +273,7 @@ CREATE TABLE `menu_category` (
 --
 
 INSERT INTO `menu_category` (`id`, `code`, `description`, `archived`) VALUES
-(1, 'breakfast', 'Breakfast Meals', 0),
+(1, 'breakfasta', 'Breakfast Mealsa', 1),
 (2, 'rice', 'Rice Meals', 0),
 (3, 'pasta', 'Pastas', 0),
 (4, 'burgers', 'Burgers', 0),
@@ -233,7 +284,8 @@ INSERT INTO `menu_category` (`id`, `code`, `description`, `archived`) VALUES
 (9, 'fruitshakes', 'Fruit Shakes', 0),
 (10, 'milkshakes', 'Milk Shakes', 0),
 (11, 'refreshments', 'Refreshments', 0),
-(12, 'coffee', 'Coffee', 0);
+(12, 'coffee', 'Coffee', 0),
+(13, 'test', 'test', 1);
 
 -- --------------------------------------------------------
 
@@ -266,6 +318,24 @@ CREATE TABLE `menu_ingredient` (
   `amount` decimal(10,0) NOT NULL,
   `archived` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menu_ingredient`
+--
+
+INSERT INTO `menu_ingredient` (`id`, `ingredientId`, `menuId`, `amount`, `archived`) VALUES
+(1, 2, 1, '4', 0),
+(2, 2, 1, '1', 1),
+(3, 3, 1, '1', 0),
+(4, 4, 1, '1', 0),
+(5, 5, 1, '1', 0),
+(6, 6, 2, '75', 0),
+(7, 2, 2, '1', 0),
+(8, 3, 2, '1', 0),
+(9, 4, 2, '1', 0),
+(10, 5, 1, '1', 0),
+(11, 2, 2, '1', 0),
+(12, 1, 1, '1', 0);
 
 -- --------------------------------------------------------
 
@@ -325,6 +395,29 @@ CREATE TABLE `orders` (
   `menuId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `status`, `paid`, `cash`, `archived`, `tableId`, `menuId`) VALUES
+(1, 'pending', 0, 0, 1, 1, 4),
+(2, 'pending', 0, 0, 1, 1, 2),
+(3, 'pending', 0, 0, 1, 1, 3),
+(4, 'pending', 0, 0, 1, 1, 4),
+(5, 'pending', 0, 0, 1, 1, 5),
+(6, 'pending', 0, 0, 0, 1, 5),
+(7, 'pending', 0, 0, 0, 1, 5),
+(8, 'pending', 0, 0, 0, 2, 2),
+(9, 'pending', 0, 0, 0, 2, 2),
+(10, 'pending', 0, 0, 0, 2, 3),
+(11, 'pending', 0, 0, 0, 2, 3),
+(12, 'pending', 0, 0, 0, 2, 9),
+(13, 'pending', 0, 0, 0, 2, 10),
+(14, 'pending', 0, 0, 0, 3, 11),
+(15, 'pending', 0, 0, 0, 3, 11),
+(16, 'pending', 0, 0, 0, 3, 11),
+(17, 'pending', 0, 0, 0, 3, 11);
+
 -- --------------------------------------------------------
 
 --
@@ -346,6 +439,29 @@ CREATE TABLE `orders_log` (
   `archived` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `orders_log`
+--
+
+INSERT INTO `orders_log` (`id`, `createdDate`, `modifiedDate`, `modifiedBy`, `action`, `orderId`, `tableId`, `menuId`, `status`, `paid`, `cash`, `archived`) VALUES
+(1, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 1, 1, 2, 'pending', 0, 0, 0),
+(2, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 2, 1, 2, 'pending', 0, 0, 0),
+(3, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 3, 1, 3, 'pending', 0, 0, 0),
+(4, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 4, 1, 4, 'pending', 0, 0, 0),
+(5, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 5, 1, 5, 'pending', 0, 0, 0),
+(6, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 6, 1, 5, 'pending', 0, 0, 0),
+(7, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 7, 1, 5, 'pending', 0, 0, 0),
+(8, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 8, 2, 2, 'pending', 0, 0, 0),
+(9, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 9, 2, 2, 'pending', 0, 0, 0),
+(10, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 10, 2, 3, 'pending', 0, 0, 0),
+(11, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 11, 2, 3, 'pending', 0, 0, 0),
+(12, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 12, 2, 9, 'pending', 0, 0, 0),
+(13, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 13, 2, 10, 'pending', 0, 0, 0),
+(14, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 14, 3, 11, 'pending', 0, 0, 0),
+(15, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 15, 3, 11, 'pending', 0, 0, 0),
+(16, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 16, 3, 11, 'pending', 0, 0, 0),
+(17, '2018-03-01 18:38:58', '2018-03-01 18:38:58', 2, 'added', 17, 3, 11, 'pending', 0, 0, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -355,33 +471,35 @@ CREATE TABLE `orders_log` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `type` varchar(64) NOT NULL
+  `userPassword` varchar(64) NOT NULL,
+  `userType` varchar(64) NOT NULL,
+  `archived` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `type`) VALUES
-(1, 'super', 'super', 'superadmin'),
-(2, 'admin', 'admin', 'admin'),
-(3, 'cashier', 'cashier', 'cashier'),
-(4, 'kitchen', 'kitchen', 'kitchen'),
-(5, 'alpha', 'alpha', 'waiter'),
-(6, 'bravo', 'bravo', 'waiter'),
-(7, 'charlie', 'charlie', 'waiter'),
-(8, 'delta', 'delta', 'waiter'),
-(9, 'echo', 'echo', 'waiter'),
-(10, 'foxtrot', 'foxtrot', 'waiter'),
-(11, 'golf', 'golf', 'waiter'),
-(12, 'hotel', 'hotel', 'waiter'),
-(13, 'india', 'india', 'waiter'),
-(14, 'juliet', 'juliet', 'waiter'),
-(15, 'kilo', 'kilo', 'waiter'),
-(16, 'lima', 'lima', 'waiter'),
-(17, 'mike', 'mike', 'waiter'),
-(18, 'november', 'november', 'waiter');
+INSERT INTO `users` (`id`, `username`, `userPassword`, `userType`, `archived`) VALUES
+(1, 'super1', 'super1', 'superadmin', 0),
+(2, 'admin', 'admin', 'admin', 0),
+(3, 'cashier', 'cashier', 'cashier', 1),
+(4, 'kitchen', 'kitchen', 'kitchen', 0),
+(5, 'alpha', 'alpha', 'waiter', 1),
+(6, 'bravo', 'bravo', 'waiter', 0),
+(7, 'charlie', 'charlie', 'waiter', 0),
+(8, 'delta', 'delta', 'waiter', 0),
+(9, 'echo', 'echo', 'waiter', 0),
+(10, 'foxtrot', 'foxtrot', 'waiter', 0),
+(11, 'golf', 'golf', 'waiter', 0),
+(12, 'hotel', 'hotel', 'waiter', 0),
+(13, 'india', 'india', 'waiter', 0),
+(14, 'juliet', 'juliet', 'waiter', 0),
+(15, 'kilo', 'kilo', 'waiter', 0),
+(16, 'lima', 'lima', 'waiter', 0),
+(17, 'mike', 'mike', 'waiter', 0),
+(18, 'november', 'november', 'waiter', 0),
+(19, 'test', 'test', 'superadmin', 0);
 
 -- --------------------------------------------------------
 
@@ -566,7 +684,7 @@ ALTER TABLE `users_log`
 -- AUTO_INCREMENT for table `customer_table`
 --
 ALTER TABLE `customer_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `customer_table_log`
@@ -578,7 +696,7 @@ ALTER TABLE `customer_table_log`
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `images_log`
@@ -590,7 +708,7 @@ ALTER TABLE `images_log`
 -- AUTO_INCREMENT for table `ingredient`
 --
 ALTER TABLE `ingredient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `ingredient_log`
@@ -608,7 +726,7 @@ ALTER TABLE `menu`
 -- AUTO_INCREMENT for table `menu_allergen`
 --
 ALTER TABLE `menu_allergen`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `menu_allergen_log`
@@ -620,7 +738,7 @@ ALTER TABLE `menu_allergen_log`
 -- AUTO_INCREMENT for table `menu_category`
 --
 ALTER TABLE `menu_category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `menu_category_log`
@@ -632,7 +750,7 @@ ALTER TABLE `menu_category_log`
 -- AUTO_INCREMENT for table `menu_ingredient`
 --
 ALTER TABLE `menu_ingredient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `menu_ingredient_log`
@@ -650,19 +768,19 @@ ALTER TABLE `menu_log`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `orders_log`
 --
 ALTER TABLE `orders_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `users_log`
